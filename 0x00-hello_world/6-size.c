51130ac2dd8ffae4569cb65cae7a9b009d83e545
Mhiretbekalu <mhiretbekalu@gmail.com>
2022-06-29 17:16:40 -0700

    S

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/*
+ *File 6-size
+ *Auth: Eual G
+ */
+
+#include <stdio.h>
+/**
+ * main - This file prints the memory size of the prmitivitive data types
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
