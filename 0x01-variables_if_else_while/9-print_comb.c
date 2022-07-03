e35080e1a46cdc7d0f78cd8b7d30fc55d685e25f
Mhiretbekalu <mhiretbekalu@gmail.com>
2022-07-02 19:17:30 -0700

    D

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 8; ++num)
+	for (num = 0; num <= 9; ++num)
 	{
 		putchar(num + '0');
-		putchar(',');
+		if (num != 9)
+			putchar(',');
 		putchar(' ');
 	}
-	putchar(9 + '0');
-
 	return (0);
 }
 
