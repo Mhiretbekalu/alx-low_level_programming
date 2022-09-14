444e16d2ec2d5f78dda941c10e5bac6c6633fa50
Mhiretbekalu <mhiretbekalu@gmail.com>
2022-09-14 08:46:36 -0700

    projects

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
+int real_sqrt_recursion(int n, int i);
+int real_prime(int n, int i);
+int _palindrome(char *s, int i, int length);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
